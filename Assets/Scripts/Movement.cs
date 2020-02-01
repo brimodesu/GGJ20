582794888d293e4aa7c397b5d732119e2b67f9b4
David Carrera <brim.carrera@gmail.com>
2020-02-01 16:46:40 -0600

    collision with material

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,8 +8,11 @@ public class Movement : MonoBehaviour
     [SerializeField] private float jumpHeight = 2f;
 
     private Vector2 i_movement;
+    private bool canInteract = false;
+    private bool canDrop = false;
+    private GameObject interactedObj;
 
-
+    public Transform pickedItemPos;
     private void Start()
     {
         Debug.Log("New Player created.");
@@ -23,18 +26,42 @@ public class Movement : MonoBehaviour
     public void OnJump()
     {
         transform.position = new Vector3(transform.position.x, transform.position.y + jumpHeight, transform.position.z);
+        if (canInteract)
+        {
+            interactedObj.GetComponent<ComponenteScript>().velocidadPublica = 0f;
+            interactedObj.transform.parent = this.gameObject.transform;
+            interactedObj.transform.position = pickedItemPos.position;
+        }
+
+        if (canDrop)
+        {
+            interactedObj.transform.parent = null;
+        }
     }
 
     public void OnMovement(InputValue value)
     {
-       /// Debug.Log("Value: " + value);
-       i_movement = value.Get<Vector2>();
+        /// Debug.Log("Value: " + value);
+        i_movement = value.Get<Vector2>();
     }
 
     public void Move()
     {
-        Vector3 movement = new Vector3(i_movement.x, 0,i_movement.y) * movementSpeed * Time.deltaTime;
+        Vector3 movement = new Vector3(i_movement.x, 0, i_movement.y) * movementSpeed * Time.deltaTime;
         transform.Translate(movement);
     }
-   
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals("Material"))
+        {
+            canInteract = true;
+            interactedObj = other.gameObject;
+        }
+
+        if (other.gameObject.tag.Equals("CintaP"))
+        {
+            canDrop = true;
+        }
+    }
 }
