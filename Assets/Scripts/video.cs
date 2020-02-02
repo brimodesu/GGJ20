8d7d08f329ed74a2d2e695c894a5a17fa6b7c4bb
JoshF8 <joshuafranco98@gmail.com>
2020-02-02 15:33:23 -0600

    sordos

---
+++ Assets/Scripts/video.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class video : MonoBehaviour
+{
+    [CanBeNull] public Camera camer_;
+
+    public VideoPlayer videop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        camer_ = Camera.main;
+        videop = this.GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
