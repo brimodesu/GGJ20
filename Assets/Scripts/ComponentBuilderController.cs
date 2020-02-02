29b69a1e1ea88932889c443a5ed1e6c6eac9e46d
David Carrera <brim.carrera@gmail.com>
2020-02-02 01:43:03 -0600

    component builder

--- Assets/Scripts/ComponentBuilderController.cs
+++ Assets/Scripts/ComponentBuilderController.cs
@@ -1,18 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Models;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ComponentBuilderController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public  List<RequiredItem> required_items = new List<RequiredItem>();
+
+    public GameObject img;
+    public Canvas Canvas;
+
+    private void Start()
     {
-        
+        foreach (var requiredItem in required_items)
+        {
+            CreateRequiredItemIndicator(requiredItem);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals("Material"))
+        {
+            bool containsItem = required_items.Any(item => item.name == other.gameObject.name);
+            Debug.Log(containsItem);
+            if (containsItem)
+            {
+                IEnumerable<RequiredItem> required_item = required_items.Where(item => item.name.Equals(other.gameObject.name));
+               
+                foreach (var item in required_item)
+                {
+                    item.image_obj.color =  new Color32(0,255,0,255);
+                }
+            }
+        }
+    }
+
+    private int offset = 0;
+    void CreateRequiredItemIndicator(RequiredItem requiredItem)
     {
         
+        var createImage = Instantiate(img) as GameObject;
+      
+        createImage.transform.position = new  Vector3(createImage.transform.position.x + offset ,createImage.transform.position.y,0f);
+        offset += 30;
+        createImage.transform.SetParent(Canvas.transform, false);
+        requiredItem.image_obj = createImage.GetComponent<Image>();
+
     }
 }
---
+++ Assets/Scripts/Models/RequiredItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Models
+{
+    [Serializable]
+    public class RequiredItem
+    {
+        public int index = 0;
+        public Sprite img;
+        public Image image_obj;
+        public string name;
+    }
+}
